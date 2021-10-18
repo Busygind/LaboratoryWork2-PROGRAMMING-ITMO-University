a49c809ffbd0e55bdda29a3689fdd3f57374a86d
Dmitriy Busygin <Busygin.voll@mail.ru>
2021-10-19 01:42:14 +0300

    Update BattleSimulator.java

--- src/BattleSimulator.java
+++ src/BattleSimulator.java
@@ -5,12 +5,12 @@ public class BattleSimulator {
 
     public static void main(String[] args) {
         Battle b = new Battle();
-        Raichu p1 = new Raichu("", 1);
-        Pikachu p2 = new Pikachu("", 1);
-        Arbok p3 = new Arbok("", 1);
-        Breloom p4 = new Breloom("", 1);
-        Regirock p5 = new Regirock("", 1);
-        Regice p6 = new Regice("", 1);
+        Raichu p1 = new Raichu("S1mple", 1);
+        Pikachu p2 = new Pikachu("electronic", 1);
+        Arbok p3 = new Arbok("b1t", 1);
+        Breloom p4 = new Breloom("Xyp9x", 1);
+        Regirock p5 = new Regirock("dupreeh", 1);
+        Regice p6 = new Regice("gla1ve", 1);
         b.addAlly(p1);
         b.addAlly(p2);
         b.addAlly(p3);
