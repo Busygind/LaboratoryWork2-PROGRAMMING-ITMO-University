f898a65f80ffbb364702b06b625afd2f855fd9d5
Dmitriy Busygin <Busygin.voll@mail.ru>
2021-10-19 01:32:33 +0300

    Update HornLeech.java

--- src/moves/HornLeech.java
+++ src/moves/HornLeech.java
@@ -8,12 +8,13 @@ public class HornLeech extends PhysicalMove {
     public HornLeech(){
         super(Type.GRASS, 75, 100);
     }
-//ИСПРАВИТЬ
-//    @Override
-//    protected void applySelfEffect(Pokemon p){
-//        int diffHP = (int) abs(p.getHP() - Stat.HP);
-//        Stat qwe = Stat.HP;
-//    }
+
+    @Override
+    protected void applySelfDamage(Pokemon p, double damage){
+        damage = -0.5*abs(p.getHP() - p.getStat(Stat.HP));
+        System.out.println("Regice восстанавливает " + -damage + " единицы здоровья");
+    }
+
     @Override
     protected String describe() {
         return "использует Horn Leech";
