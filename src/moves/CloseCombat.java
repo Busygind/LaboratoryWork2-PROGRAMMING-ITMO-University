a6b81378d1eae7c724735a5fa7b38d4323dcdafb
Dmitriy Busygin <Busygin.voll@mail.ru>
2021-10-19 01:32:31 +0300

    Update CloseCombat.java

--- src/moves/CloseCombat.java
+++ src/moves/CloseCombat.java
@@ -7,11 +7,11 @@ public class CloseCombat extends PhysicalMove {
         super(Type.FIGHTING, 120, 100);
     }
 
-//    @Override
-//    protected void applySelfEffect(Pokemon p){
-//        p.setMod(Stat.DEFENSE, -1);
-//        p.setMod(Stat.SPECIAL_DEFENSE, -1);
-//    }
+    @Override
+    protected void applySelfEffects(Pokemon p){
+        p.setMod(Stat.DEFENSE, -1);
+        p.setMod(Stat.SPECIAL_DEFENSE, -1);
+    }
 
     @Override
     protected String describe() {
